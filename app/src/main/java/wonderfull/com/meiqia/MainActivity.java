483e53c9dcefc072073ac6ba9192d5f1cd9c60cd
sunruichuan <sunruichuan@inagora.cn>
2016-04-19 15:14:39 +0800

    增加美洽SDK依賴

--- app/src/main/java/wonderfull/com/meiqia/MainActivity.java
+++ app/src/main/java/wonderfull/com/meiqia/MainActivity.java
@@ -2,12 +2,19 @@ package wonderfull.com.meiqia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
